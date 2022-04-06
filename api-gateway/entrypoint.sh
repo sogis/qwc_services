7fa78db3ae013fd216c94eee53090099da26291b
bjsvwpfe <michael.pfeiffer@bd.so.ch>
2022-04-06 18:23:59 +0200

    try to enable filebeat

--- api-gateway/entrypoint.sh
+++ api-gateway/entrypoint.sh
@@ -26,46 +26,10 @@ nginx -g "daemon off;" &
 
 nginx_pid=$!
 
-test -n "${API_KEY}" && \
-    api_key=${API_KEY}
-
-test -n "${AMPLIFY_IMAGENAME}" && \
-    amplify_imagename=${AMPLIFY_IMAGENAME}
-
-if [ -n "${api_key}" -o -n "${amplify_imagename}" ]; then
-    echo "updating ${agent_conf_file} ..."
-
-    if [ ! -f "${agent_conf_file}" ]; then
-	test -f "${agent_conf_file}.default" && \
-	cp -p "${agent_conf_file}.default" "${agent_conf_file}" || \
-	{ echo "no ${agent_conf_file}.default found! exiting."; exit 1; }
-    fi
-
-    test -n "${api_key}" && \
-    echo " ---> using api_key = ${api_key}" && \
-    sh -c "sed -i.old -e 's/api_key.*$/api_key = $api_key/' \
-	${agent_conf_file}"
-
-    test -n "${amplify_imagename}" && \
-    echo " ---> using imagename = ${amplify_imagename}" && \
-    sh -c "sed -i.old -e 's/imagename.*$/imagename = $amplify_imagename/' \
-	${agent_conf_file}"
-
-    test -f "${agent_conf_file}" && \
-    chmod 644 ${agent_conf_file} && \
-    chown nginx ${agent_conf_file} > /dev/null 2>&1
-
-    test -f "${nginx_status_conf}" && \
-    chmod 644 ${nginx_status_conf} && \
-    chown nginx ${nginx_status_conf} > /dev/null 2>&1
-fi
-
-if ! grep '^api_key.*=[ ]*[[:alnum:]].*' ${agent_conf_file} > /dev/null 2>&1; then
-    echo "no api_key found in ${agent_conf_file}! exiting."
-fi
 
 echo "starting amplify-agent ..."
-service amplify-agent start > /dev/null 2>&1 < /dev/null
+systemctl enable filebeat > /dev/null 2>&1
+systemctl start filebeat > /dev/null 2>&1
 
 if [ $? != 0 ]; then
     echo "couldn't start the agent, please check ${agent_log_file}"
